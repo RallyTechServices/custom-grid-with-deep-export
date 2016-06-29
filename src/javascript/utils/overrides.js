d1dfac632d32493b7e92d391cc2588c7b2ec009c
Kristy Corkan <kcorkan@gmail.com>
2016-06-29 17:09:09 -0600

    upgraded filter, SDK version and whitelisted milestones for filtering

---
+++ src/javascript/utils/overrides.js
@@ -0,0 +1,25 @@
+Ext.override(Rally.ui.gridboard.plugin.GridBoardFieldPicker, {
+    gridFieldBlackList: [
+        'Actuals',
+        'Changesets',
+        'Children',
+       // 'Description',
+       // 'Notes',
+        'ObjectID',
+        'Predecessors',
+        'RevisionHistory',
+        'Subscription',
+        'Successors',
+        'TaskIndex',
+        'Workspace',
+        'VersionId'
+    ]
+});
+
+Ext.override(Rally.ui.inlinefilter.PropertyFieldComboBox, {
+    /**
+     * @cfg {String[]} whiteListFields
+     * field names that should be included from the filter row field combobox
+     */
+    defaultWhiteListFields: ['Milestones']
+});
