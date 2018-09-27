8f084337143a9c5d1bbd26ac035d78b7589035f6
Tyr Johanson <tyr@ca.com>
2018-09-27 22:18:07 +0000

    removed unused code

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -431,52 +431,5 @@ Ext.define("custom-grid-with-deep-export", {
         return Rally.technicalservices.CustomGridWithDeepExportSettings.getFields({
             showSearchAllProjects: this.isMilestoneScoped()
         });
-    },/*
-    fetchPortfolioItemTypes: function(){
-        var deferred = Ext.create('Deft.Deferred');
-
-        var store = Ext.create('Rally.data.wsapi.Store', {
-            model: 'TypeDefinition',
-            fetch: ['TypePath', 'Ordinal','Name'],
-            filters: [
-                {
-                    property: 'Parent.Name',
-                    operator: '=',
-                    value: 'Portfolio Item'
-                },
-                {
-                    property: 'Creatable',
-                    operator: '=',
-                    value: 'true'
-                }
-            ],
-            sorters: [{
-                property: 'Ordinal',
-                direction: 'ASC'
-            }]
-        });
-        store.load({
-            callback: function(records, operation, success){
-
-                if (success){
-                    var portfolioItemTypes = new Array(records.length);
-                    _.each(records, function(d){
-                        //Use ordinal to make sure the lowest level portfolio item type is the first in the array.
-                        var idx = Number(d.get('Ordinal'));
-                        portfolioItemTypes[idx] = { typePath: d.get('TypePath').toLowerCase(), name: d.get('Name') };
-                        //portfolioItemTypes.reverse();
-                    });
-                    deferred.resolve(portfolioItemTypes);
-                } else {
-                    var error_msg = '';
-                    if (operation && operation.error && operation.error.errors){
-                        error_msg = operation.error.errors.join(',');
-                    }
-                    deferred.reject('Error loading Portfolio Item Types:  ' + error_msg);
-                }
-            }
-        });
-        return deferred.promise;
     }
-    */
 });
