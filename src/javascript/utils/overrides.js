27c7037058787f32961feb2bff97e29c4a994356
Tyr Johanson <tyr@ca.com>
2018-10-18 21:56:08 +0000

    move PreliminaryEstimate editor fix into ancestor in-line filter

--- src/javascript/utils/overrides.js
+++ src/javascript/utils/overrides.js
@@ -1,29 +1,3 @@
-// Fix the PreliminaryEstimate renderer to sort by value
-Rally.ui.renderer.GridEditorFactory.editorRenderers['PreliminaryEstimate'] = function(field) {
-    return {
-        xtype: 'rallyrecordcontexteditor',
-        field: {
-            xtype: 'rallycombobox',
-            allowNoEntry: !field.required,
-            editable: false,
-            name: field.name,
-            storeConfig: {
-                autoLoad: true,
-                model: field.name,
-                remoteFilter: true,
-                sorters: [{
-                    property: "Value"
-                }],
-                listeners: {
-                    load: function() {
-                        return;
-                    }
-                }
-            }
-        }
-    };
-};
-
 // Remove 'Actuals' from the blacklist
 Ext.override(Rally.ui.gridboard.plugin.GridBoardFieldPicker, {
     gridFieldBlackList: [
